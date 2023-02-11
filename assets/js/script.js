ec1f9fc3b0cbab488db325aff5dc357ee4148ce9
Terry-Martin <terry.martin@gmail.com>
2023-02-11 16:30:21 +0000

    Edited division game element

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operand1').textContent = (Math.floor(Math.random() * 25) + 1) * operand2;
     document.getElementById('operator').textContent = "/";
 }
