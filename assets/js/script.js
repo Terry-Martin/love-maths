b6ae0e3c4479ae8792eff9cc5c1fb0f0579a32f9
Terry-Martin <terry.martin@gmail.com>
2023-02-11 16:21:05 +0000

    Edited subtraction game element

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,8 +133,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
