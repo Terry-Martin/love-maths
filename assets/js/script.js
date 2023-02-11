0f5b588b13822fe49e891e39ac6c0b3e8ef6769b
Terry-Martin <terry.martin@gmail.com>
2023-02-11 15:20:50 +0000

    Check user answer against correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,39 +34,72 @@ function runGame(gameType) {
         alert(`Unknown game type: ${gameType}`);
         throw (`Unknown game type: ${gameType}. Aborting!`);
     }
-            
+
 }
+
+
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("You da man!")
+    } else {
+        alert(`Unlucky - you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+
 }
 
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
 
-}
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
-function incrementScore() {
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]; 
+        } else {
+                alert(`Unimplemented operaotor ${operator}`);
+                throw `Unimplemented operaotor ${operator}. Aborting!`;
+            }
+        }
 
-}
+    function incrementScore() {
 
-function incrementWrongAnswer() {
+    }
 
-}
+    function incrementWrongAnswer() {
 
-function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-}
+    }
 
-function displaySubtractQuestion() {
+    function displayAdditionQuestion(operand1, operand2) {
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
+    }
 
-}
+    function displaySubtractQuestion() {
 
-function displayMultiplyQuestion() {
+    }
 
-}
+    function displayMultiplyQuestion() {
 
-function displayDivisionQuestion() {
+    }
 
-}
+    function displayDivisionQuestion() {
+
+    }
